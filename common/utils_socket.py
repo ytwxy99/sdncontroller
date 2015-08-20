7af5ad99e1309c2177b3f012b757c454f5b655c6
Wang Xiaoyu <ytwxy99@yahoo.com>
2015-08-20 01:35:19 -0700

    upload utils_socket.py file

---
+++ common/utils_socket.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+def create_new_socket(block):
+    """Get a socket hock """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setblocking(block)
+    return sock
