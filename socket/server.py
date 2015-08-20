ff22ae56f9d98f0bc944c18fcbc98920934b5ac0
Wang Xiaoyu <ytwxy99@yahoo.com>
2015-08-20 02:07:47 -0700

    modified server.py

--- socket/server.py
+++ socket/server.py
@@ -8,17 +8,17 @@ import time
 from sdncontroller.common import utils_socket
 
 if __name__ == '__main__':
-	sock = utils_socket.create_new_socket(0)
-	sock.bind(("", 6634))
-	sock.listen(30)
+    sock = utils_socket.create_new_socket(0)
+    sock.bind(("", 6634))
+    sock.listen(30)
 	
-	io_loop = ioloop.IOLoop.instance()
+    io_loop = ioloop.IOLoop.instance()
 	
-	callback = functools.partial(agent, sock)
-	print sock, sock.getsockname()
-	io_loop.add_handler(sock.fileno(), callback, io_loop.READ)
-	try:
-		io_loop.start()
-	except KeyboardInterrupt:
-		io_loop.stop()
-		print "quit"
+    callback = functools.partial(agent, sock)
+    print sock, sock.getsockname()
+    io_loop.add_handler(sock.fileno(), callback, io_loop.READ)
+    try: 
+        io_loop.start()
+    except KeyboardInterrupt: 
+        io_loop.stop()
+        print "quit"
