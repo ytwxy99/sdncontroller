83a594a35eab2abfdbf9d3cb0cd660b57725f957
Wang Xiaoyu <ytwxy99@yahoo.com>
2015-08-20 03:06:41 -0700

    modified server.py

--- socket/server.py
+++ socket/server.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import errno
 import functools
 import tornado.ioloop as ioloop
-import Queue
-import time
 from sdncontroller.common import utils_socket
 
 def socket_server():
@@ -17,19 +14,38 @@ def socket_server():
     # To listen request from client service
     sock.listen(30)
     
+    #print sock, sock.getsockname()
     # Get a IOLoop instance	
     io_loop = ioloop.IOLoop.instance()
 
     # Using functools's modal to genrate a callback function	
-    callback = functools.partial(agent, sock)
-    print sock, sock.getsockname()
+    callback = functools.partial(socket_server_accept, sock, io_loop)
+    # Help information: add_handler(self, fd, handler, events) from help(io_loop)
     # Add handler by io_loop
     io_loop.add_handler(sock.fileno(), callback, io_loop.READ)
     try: 
+        # Start IO loop
         io_loop.start()
     except KeyboardInterrupt: 
+        # Stop IO loop
         io_loop.stop()
-        print "quit"
+        print "\n Quite"
+
+def socket_server_accept(sock, io_loop, fd, events):
+    print fd, sock, events
+    try:
+        connection, address = sock.accept()
+    except socket.error, e:
+        if e.args[0] not in (errno.EWOULDBLOCK, errno.EAGAIN):
+            raise
+        return
+    connection.setblocking(0)
+    handle_connection(connection, address)
+    fd_map[connection.fileno()] = connection
+    client_handle = functools.partial(client_handler, address)
+    io_loop.add_handler(connection.fileno(), client_handle, io_loop.READ)
+    print "i n agent: new switch", connection.fileno(), client_handle
+    message_queue_map[connection] = Queue.Queue()
 
 if __name__ == '__main__':
     socket_server()
