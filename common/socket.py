14c5c9292bfb56f863b435bf243a0a23534b942d
Wang Xiaoyu <ytwxy99@yahoo.com>
2015-08-19 23:59:23 -0700

    upload sokect.py file for sokect's correlative funcitons

--- common/socket.py
+++ common/socket.py
@@ -3,8 +3,8 @@
 import socket
 
 def create_new_socket():
-	"""Get a socket hock """
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    """Get a socket hock """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setblocking(block)
     return sock
